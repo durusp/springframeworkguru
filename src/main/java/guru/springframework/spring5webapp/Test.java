4b65b23ae4613211b64a5bf7749a49dcde2d85b6
durusp <durus.ruxandra@yahoo.com>
2019-06-22 23:46:18 +0300

    guru files controllers

---
+++ src/main/java/guru/springframework/spring5webapp/Test.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring5webapp;
+
+public class Test {
+    public static void main(String[] args) {
+        Object obj = null;
+        System.out.println(obj instanceof String);
+    }
+}
